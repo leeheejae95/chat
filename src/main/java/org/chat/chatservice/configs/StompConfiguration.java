03a814183db49e2bae476829ba8a9ac02ee3994c
leehj <leeheejae55@naver.com>
2026-01-02 16:24:04 +0900

    fix: 프론트엔드 호출 규격에 맞춰 채팅 API 경로 수정

--- src/main/java/org/chat/chatservice/configs/StompConfiguration.java
+++ src/main/java/org/chat/chatservice/configs/StompConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class StompConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) { // 클라이언트가 어떤 URL로 WebSocket 연결을 시도할지 결정
-        registry.addEndpoint("/stomp/chat");
+        registry.addEndpoint("/stomp/chats");
     }
 
     @Override
